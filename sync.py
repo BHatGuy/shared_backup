3e852f932276f151d5e49d91f5b134f9cc57a4a6
Malte Müller <malte@malte-mueller.eu>
2021-10-11 22:38:21 +0200

    First version??

--- sync.py
+++ sync.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from os import path
+import os
 import subprocess as sp
 import yaml
 import logging as log
@@ -11,6 +11,7 @@ log.basicConfig(
 )
 
 PREFIX = "/data/"
+config = None
 
 
 def mount():
@@ -18,31 +19,90 @@ def mount():
 
     cmd_key = ["ecryptfs-add-passphrase"]
     log.debug(f"Executing {cmd_key}")
-    r = sp.run(cmd_key, input=config["ecryptfs_passphrase"].encode(), stdout=sp.DEVNULL)  
+    r = sp.run(cmd_key, input=config["ecryptfs_passphrase"].encode(), stdout=sp.DEVNULL)
     if r.returncode != 0:
         log.error("Error while mounting!")
         quit(-1)
 
-    cmd_mount = ["mount", "-i", path.join(PREFIX, config["username"], config["dec_dir"])]
+    cmd_mount = [
+        "mount",
+        "-i",
+        os.path.join(PREFIX, config["username"], config["dec_dir"]),
+    ]
     log.debug(f"Executing {cmd_mount}")
-    r = sp.run(cmd_mount, stdout=sp.DEVNULL)  
+    r = sp.run(cmd_mount, stdout=sp.DEVNULL)
     if r.returncode != 0:
         log.error("Error while mounting!")
         quit(-1)
+
+    log.info("Mounting successfull")
+
 
 def unmount():
-    pass
+    log.info("Unmounting...")
+
+    cmd = ["umount", os.path.join(PREFIX, config["username"], config["dec_dir"])]
+    log.debug(f"Executing {cmd}")
+    r = sp.run(cmd, stdout=sp.DEVNULL)
+    if r.returncode != 0:
+        log.error("Error while unmounting!")
+        quit(-1)
+
+    log.info("Unmounting successfull")
 
 
-with open("config.yml", "r") as config_file:  #  TODO as parameter
-    config = yaml.safe_load(config_file)
-if config is None:
-    log.error("Could not load config")
-    quit(-1)
-if config["debug"]:
-    log.getLogger().setLevel(log.DEBUG)
-log.info("Loaded config")
-log.debug(f"Config: {config}")
+def sync_local():
+    log.info("Updating local copy")
+    for dir in config["dirs"]:
+        log.info(f"Updating {dir} locally")
+        path = os.path.join(PREFIX, config["username"], config["dirs"][dir]["path"])
+        dest = os.path.join(PREFIX, config["username"], config["dec_dir"])
+        excludes = config["dirs"][dir]["exclude"]
+        cmd = ["rsync", "-a"]  # TODO other parameters
+        for e in excludes:
+            cmd.append("--exclude=" + e)
+        cmd += [path, dest]
+        log.debug(f"Executing {cmd}")
+        r = sp.run(cmd, stdout=sp.DEVNULL)
+        if r.returncode != 0:
+            log.error(f"Error while Updating {dir} locally")
+            quit(-1)
 
-mount()
-unmount()
+
+def sync_remote():
+    log.info("Syncing to peers")
+    for peer in config["peers"]:
+        ip = config["ip_prefix"] + str(peer)
+        log.info(f"Syncing to {ip}")
+        path = os.path.join(PREFIX, config["username"], config["enc_dir"]) + "/"
+        uname = config["username"]
+        remote = f"{uname}@{ip}:{path}"
+        cmd = ["rsync", "--timeout=15", "-az", path, remote]  # TODO other parameters
+        log.debug(f"Executing {cmd}")
+        r = sp.run(cmd, stdout=sp.DEVNULL)
+        if r.returncode == 30:
+            log.warning(f"Connection timed out, trying next peer")
+        elif r.returncode != 0:
+            log.error(f"Error syncing to peer {ip}")
+            quit(-1)
+
+
+def load_config():
+    with open("config.yml", "r") as config_file:  #  TODO as parameter
+        config = yaml.safe_load(config_file)
+    if config is None:
+        log.error("Could not load config")
+        quit(-1)
+    if config["debug"]:
+        log.getLogger().setLevel(log.DEBUG)
+    log.info("Loaded config")
+    log.debug(f"Config: {config}")
+    return config
+
+
+if __name__ == "__main__":
+    config = load_config()
+    mount()
+    sync_local()
+    unmount()
+    sync_remote()
